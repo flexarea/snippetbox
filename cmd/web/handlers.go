76f70a57e6f34878693d18c3afe37899052f1d57
esdras <esdrasntuyenabo40@gmail.com>
2024-07-02 14:59:50 +0200

    created fileserver

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,10 +13,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
 		http.Error(w, "Internal server error", 500)
 		return
 	}
@@ -24,7 +29,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	err = ts.Execute(w, nil)
 
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
 		http.Error(w, "Internal server error", 500)
 	}
 
